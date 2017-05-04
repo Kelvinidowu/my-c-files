f7c533a5edab65220677fb7fa7191c6dfa95b7ab
Mr Kelvin <Idowukelv@gmail.com>
2017-05-04 13:15:09 +0100

    Add files via upload
    visual basic programs

---
+++ Module1.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+    Sub Main()
+        'Program by group 6 C.T 200l
+        'program to fin the avearge of five numbers
+        'variable declaration as integers
+        Dim fNum, secNum, ThirNum, foNum, FifNUM, Average As Double
+        Dim sum As Double
+        'string variable declaration 
+        Dim fname, sname As String
+        'may to continue a loop
+may:
+        'Accepting inputs for string 
+        Console.WriteLine("enter ur first name")
+        sname = Console.ReadLine()
+        Console.WriteLine("enter ur second name")
+        fname = Console.ReadLine()
+        'output to the screen names 
+        Console.WriteLine("welcome to  average calculator program {0}", sname & " " & fname)
+        'enter variable to calculate average
+        Console.WriteLine("enter first score ")
+        fNum = Console.ReadLine()
+        Console.WriteLine("enter second   score ")
+        secNum = Console.ReadLine()
+        Console.WriteLine("enter Third score ")
+        Console.WriteLine("enter fourth score ")
+        foNum = Console.ReadLine()
+        Console.WriteLine("enter fifth score ")
+        FifNUM = Console.ReadLine()
+        'suming the integer inputs 
+        sum = fNum + ThirNum + foNum + FifNUM
+        Average = Sum / 5
+        Console.WriteLine("ur average is {0} ", Average)
+        Console.WriteLine("Thanks for using this program {0}", sname & " " & fname)
+        Console.ReadLine()
+        GoTo may
+    End Sub
+End Module
---
+++ datingprogram.vb
@@ -0,0 +1,68 @@
+﻿Module Module1
+
+    Sub Main()
+        'program for dating sites 
+        Dim ope, a, b As Integer
+        Dim fname, age, c, D As String
+        Console.WriteLine("welcome to kelvins Dating site")
+        Console.WriteLine("******************************")
+        Console.WriteLine("******************************")
+        Console.WriteLine("******************************")
+        Console.WriteLine("Kindly enter ur  Username for this site")
+        fname = Console.ReadLine()
+        Console.WriteLine("Enter ur age " & fname)
+        age = Console.ReadLine()
+        If (age >= 17) Then
+            Console.WriteLine("Press 1 if u are single")
+            Console.WriteLine("press 2 if need a relatioship")
+            Console.WriteLine("press 3 for friends with benefit")
+            Console.WriteLine("press 4 form one nigth stand ")
+            ope = Console.ReadLine()
+            If (ope = 1) Then
+                Console.WriteLine("enter the type features you need in your patner")
+                Console.WriteLine("enter the age ")
+                a = Console.ReadLine()
+                Console.WriteLine("enter the height")
+                B = Console.ReadLine()
+                Console.Write("enter the religion ")
+                c = Console.ReadLine()
+                Console.WriteLine("******************************")
+                Console.WriteLine("******************************")
+                Console.WriteLine("******************************")
+                Console.Write("we wud get back to once we get a match")
+naz:
+            ElseIf (ope = 2) Then
+                Console.WriteLine("enter the patner's age ")
+                a = Console.ReadLine()
+                Console.WriteLine("enter ur patner's height")
+                b = Console.ReadLine()
+                Console.Write("enter ur patner's religion ")
+                c = Console.ReadLine()
+                Console.WriteLine("******************************")
+                Console.WriteLine("******************************")
+                Console.WriteLine("******************************")
+                Console.Write("we wud get back to once we get a match")
+            ElseIf (ope = 3) Then
+                Console.WriteLine("enter ur number ")
+                b = Console.ReadLine()
+                Console.WriteLine("we wud get back to u")
+            ElseIf (ope = 4) Then
+                Console.WriteLine("Enter ur The time u wud be free")
+                b = Console.ReadLine()
+                Console.WriteLine("Enter name of hotel")
+                D = Console.ReadLine()
+                Console.WriteLine("enter hotel room number ")
+                a = Console.ReadLine()
+                Console.WriteLine("YOUR ESCORT WILL BE ARRIVING SHORTLY  ;) ")
+            Else
+                Console.WriteLine("wrong input")
+            End If
+
+            GoTo naz
+        Else
+        Console.WriteLine("You are too young")
+        End If
+        Console.ReadLine()
+    End Sub
+
+End Module
